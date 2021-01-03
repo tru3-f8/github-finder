f914b83c65b4da0e91f40a98816a2b71f6739152
Johnny <jliang61987@gmail.com>
2021-01-02 18:38:10 -0800

    Github Finder

--- src/App.js
+++ src/App.js
@@ -13,6 +13,7 @@ class App extends Component {
   state = {
     users: [],
     user: {},
+    repos: [],
     loading: false,
     alert: null
   }
@@ -35,6 +36,9 @@ class App extends Component {
     .get(`https://api.github.com/search/users?q=${text}&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRECT}`);
     
     this.setState({ users: res.data.items, loading: false });
+
+    //Checking if data is being fetched by console logging it
+    //console.log(res.data);
   };
 
   // Get single Github user
@@ -46,6 +50,17 @@ class App extends Component {
     
     this.setState({ user: res.data, loading: false });
   };
+  
+  // Get users repos
+  getUserRepos = async username => {
+    this.setState({ loading: true });
+    
+    const res = await axios
+    .get(`https://api.github.com/users/${username}/repos?per_page=5&sort=created:asc&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRECT}`);
+    
+    this.setState({ repos: res.data, loading: false });
+  };
+  
 
   // Clear users from state
   clearUsers = () => this.setState({ users: [], loading: false });
@@ -59,7 +74,7 @@ class App extends Component {
   }
 
   render() {
-    const { users, user, loading } = this.state;
+    const { users, user, repos, loading } = this.state;
 
     return (
       <Router>
@@ -76,7 +91,7 @@ class App extends Component {
         )} />
         <Route exact path='/about' component={About} />
         <Route exact path='/user/:login' render={props => (
-        <User { ...props } getUser={this.getUser} user={user} loading={loading} />
+        <User { ...props } getUser={this.getUser} getUserRepos={this.getUserRepos} user={user} repos={repos} loading={loading} />
         )} />
         </Switch>
         </div>
